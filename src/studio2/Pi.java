86a5a5dc245da7b08c7d139daa3adba13df307b0
azhu8 <azhu8@MandyBear.dhcp.wustl.edu>
2023-02-02 15:46:02 -0600

    hehe

---
+++ src/studio2/Pi.java
@@ -0,0 +1,14 @@
+package studio2;
+
+public class Pi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double area = Math.random();
+		double x = Math.sqrt(area);
+		double areay = Math.random();
+		double y = Math.sqrt(areay);
+		System.out.print(x + " " + y);
+	}
+
+}
